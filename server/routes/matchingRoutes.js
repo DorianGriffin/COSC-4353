c41c756042bd005639a5f5fc9648dfa1f9b0bf5c
israelmumay <112601448+israelmumay@users.noreply.github.com>
2025-08-06 18:13:01 -0500

    Update matchingRoutes.js
    new

--- server/routes/matchingRoutes.js
+++ server/routes/matchingRoutes.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const matchingController = require('../controllers/matchingController');
 
-// Correct dynamic route
-
-router.get('/user/:userId/assignments', matchingController.getUserAssignments);
-
-
 router.get('/admin/matches', matchingController.getAllMatches);
 router.get('/match/:userId', matchingController.ismatched);
 // POST accept, cancel and complete
